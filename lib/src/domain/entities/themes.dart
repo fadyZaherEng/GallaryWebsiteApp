3c72e2ac33017e5f7c4517b022a7b569c9c25903
Fady <fedo.zaher@gmail.com>
2024-09-10 11:32:58 +0300

    image dialog

--- lib/src/domain/entities/themes.dart
+++ lib/src/domain/entities/themes.dart
@@ -5,11 +5,13 @@ class MyThemes {
       scaffoldBackgroundColor: Colors.black,
       colorScheme: const ColorScheme.dark(),
       primarySwatch: Colors.deepPurple,
+      backgroundColor: Colors.white,
       cardColor: Colors.grey[900]);
 
   static final lightTheme = ThemeData(
       scaffoldBackgroundColor: Colors.white,
       colorScheme: const ColorScheme.light(),
       primaryColor: Colors.purple,
+      backgroundColor: Colors.white,
       cardColor: Colors.white);
 }
